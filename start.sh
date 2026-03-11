9f92100ab1ac585b87edea4c05da0ce92e13dee3
Sabry Dawood <kazouya25@gmail.com>
2026-03-11 23:50:49 +0200

    chore(deploy): add git pull and switch to PM2 restart in startup script

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 cd /home/Work/DeployCenter
+# Git Pull
+echo "[Deploy Center] Pulling latest changes..."
+git pull
 
+# Build
 echo "[Deploy Center] Building TypeScript..."
 npm run build
-
+# Check if build was successful
 if [ $? -ne 0 ]; then
   echo "[Deploy Center] Build failed!"
   exit 1
 fi
-
-echo "[Deploy Center] Starting server..."
-exec node -r ./tsconfig-paths-bootstrap.js dist/index.js
+# Restart PM2
+echo "[Deploy Center] Restarting server..."
+pm2 restart deploy-center
